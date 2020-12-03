b2858ca2b8abd460f33e9c57996dde4b84255dcd
James Doss-Gollin <james.doss-gollin@columbia.edu>
2020-12-03 11:34:38 -0500

    don't call things `map`, it's a buildin name

--- 03_toboggan_trajectory.jl
+++ 03_toboggan_trajectory.jl
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2020/day/3
 
+"""A tree map -- probably unnecessary here"""
 struct TreeMap
     width::Int
     height::Int
@@ -18,15 +19,15 @@ demo_map = TreeMap("demo/03.txt")
 input_map = TreeMap("inputs/03.txt")
 
 """Slope is defined so that positive is (down, right). Thus (1, 1) is top left."""
-function count_trees(map::TreeMap, slope::Tuple{Int,Int})
+function count_trees(tmap::TreeMap, slope::Tuple{Int,Int})
     x, y = 1, 1
-    hits = 0
-    while y < map.height
+    hits = tmap.obs[y, x]
+    while y < tmap.height
         x, y = (x, y) .+ slope
-        while x > map.width
-            x -= map.width # arboreal genetics and biome instability: trees repeat
+        while x > tmap.width
+            x -= tmap.width # arboreal genetics and biome instability: trees repeat
         end
-        hits += map.obs[y, x]
+        hits += tmap.obs[y, x]
     end
     return hits
 end
@@ -35,19 +36,13 @@ end
 solution_01 = count_trees(input_map, (3, 1))
 @show solution_01;
 
-possible_slopes = [
-    (1, 1),
-    (3, 1),
-    (5, 1),
-    (7, 1),
-    (1, 2),
-]
-
-function slope_product(map::TreeMap, slopes::Array{Tuple{Int,Int}})
-    hits = [count_trees(map, slope) for slope in slopes]
+"""Solution to part 02"""
+function slope_product(tmap::TreeMap, slopes::Array{Tuple{Int,Int}})
+    hits = [count_trees(tmap, slope) for slope in slopes]
     return prod(hits)
 end
 
+possible_slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 @assert slope_product(demo_map, possible_slopes) == 336
 solution_02 = slope_product(input_map, possible_slopes)
 @show solution_02;
